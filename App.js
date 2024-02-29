0692d4d1838f2e113948316101246102998abf8e
Mohammed Faisal <mohammedelormf@gmail.com>
2024-02-29 20:42:00 +0000

    app

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 
 const App = () => {
-  return (
-    <View style ={{backgroundColor: "#0DCDAA", flex: 1, justifyContent: "center", alignItems: "center"}}>
-    
-      <Text style={{fontWeight: "bold" , fontSize: 70, justifyContent: "center", color: "white"}}>Flut</Text>
-    </View>
-  )
+  return <View>
+    {/* Welcome And Image Section */}
+<View>
+  <Text>Welcome!</Text>
+  <Text>Sign in or create a new account</Text>
+<Image
+  source={require("./assets/Frame.png")}
+/>
+
+</View>
+
+    {/* Sign In And Sign Up Section */}
+  </View>
 }
 
 export default App
---
+++ src/screen/SplashScreen.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SplashScreen = () => {
+  return (
+    <View style ={{backgroundColor: "#0DCDAA", flex: 1, justifyContent: "center", alignItems: "center"}}>
+    
+      <Text style={{fontWeight: "bold" , fontSize: 70, justifyContent: "center", color: "white"}}>Flut</Text>
+    </View>
+  )
+}
+
+export default SplashScreen;
+
